24dc1985e3e6d676b8c2ed8a862cac0e54bc6a48
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-30 15:25:54 -0300

    Create aula15.py

---
+++ pythonTeste/aula15.py
@@ -0,0 +1,10 @@
+num = soma = 0
+while True:
+    num = int(input('Digite um número: '))
+    if num == 999:
+        break
+    soma += num
+
+# print('A soma vale {}'.format(soma))
+print(f'A soma vale {soma}')
+        
